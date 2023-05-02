065db6ef8597e850330940257e33c3dfa63f8d6f
abbmarand <markus.andersson@abbgymnasiet.se>
2023-05-02 15:06:03 +0200

    scraping item data for single items now works

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-import { launch } from 'puppeteer'
+import puppeteer from "puppeteer";
 
-(async () => {
-    const browser = await launch()
-    const page = await browser.newPage()
-    await page.goto('https://rustlabs.com/item/assault-rifle#tab=craft')
+async function scrapeCraftCost(page) {
+    const typeElements = await page.$$('a.item-box img');
+    const amountElements = await page.$$('a.item-box span.text-in-icon');
 
-    const footerHtml = await page.$eval('#two-column-wrap #left-column .tab-block .tab-page .table tbody tr', element => element.innerHTML)
-    console.log(footerHtml)
-    await page.screenshot({ path: 'screenshot.png' })
-    await browser.close()
-})()
+    const craftCost = [];
+
+    for (let i = 0; i < typeElements.length; i++) {
+        const typeElement = typeElements[i];
+        const amountElement = amountElements[i];
+
+        const type = await page.evaluate(typeElement => typeElement.getAttribute('alt'), typeElement);//the alt of the image is the ingame item name
+        let amount = await page.evaluate(amountElement => amountElement.innerText, amountElement);
+        if (amount === '') {
+            amount = 'x1'
+        }
+        amount = amount.substring(1)//remove the x so it can be parsed into a number
+        amount = parseInt(amount, 10)
+        if (isNaN(amount)) {//on this website the workbench tier comes after the items requred to craft and the tier is displayed like this (III) which is not parseble into an int
+            break;//this means that breaking when that is true will return only the items that are requred to craft it
+        }
+        craftCost.push({ type, amount });
+    }
+    console.log(craftCost)
+    return craftCost;
+}
+const getItemByName = async (name) => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+    const page = await browser.newPage();
+    await page.goto(`https://rustlabs.com/item/${name}#tab=craft`, {
+        waitUntil: "domcontentloaded",
+    });
+    scrapeCraftCost(page)
+};
+getItemByName("chainlink-fence")//try assult-rifle or chainlink-fence
