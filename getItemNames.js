163ee7b52c426ee07e08ea764a0f66c39f16d1b4
abbmarand <markus.andersson@abbgymnasiet.se>
2023-05-05 18:32:57 +0200

    fixed é letter and "---" for urls

--- getItemNames.js
+++ getItemNames.js
@@ -1,11 +1,14 @@
-function parseItemString (str) {
-    const lowerstr = str.toLowerCase()
-    const replaced = lowerstr.replace(/\s+/g, '-')
+function parseItemString(str) {
+    const lowerstr = str.toLowerCase()//.normalize('NFC')
+    let replaced = lowerstr.replace(/\s+/g, '-')
+    replaced = replaced.replace(/-+/g, '-'); // Replaces multiple hyphens with a single hyphen
+    replaced = replaced.replace(/é/g, 'e'); // Replaces é with e
+    console.log(replaced)
     return replaced
 }
 
 
-async function ScrapeItems (page) {
+async function ScrapeItems(page) {
     const itemarr = await page.$$('span.r-cell')
     let itemtext
     let itemtextarr = []
@@ -18,7 +21,7 @@ async function ScrapeItems (page) {
 }
 
 
-async function getItemNames (browser) {
+async function getItemNames(browser) {
     const page = await browser.newPage()
     await page.goto(`https://rustlabs.com/group=itemlist`, {
         waitUntil: "domcontentloaded",
